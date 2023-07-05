3746f80ed0664a7f8b57c89903659081016d82a2
PetrVasyutik <vasutik@mail.ru>
2023-07-05 19:19:22 +0300

    init

---
+++ source/js/main.js
@@ -0,0 +1,82 @@
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
+import { initAccordions } from './modules/accordions/init-accordion';
+import { initPhoneMask } from './modules/phone-mask/phone-mask';
+import { initAutoResizeTextarea } from './modules/textarea/auto-resize-textarea';
+import { FocusLock } from './modules/autofocus/focus-lock';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+    const form = new Form();
+    window.form = form;
+    form.init();
+
+    initAccordions();
+    initPhoneMask();
+    initAutoResizeTextarea();
+
+
+    //записываем DOM элемент в переменную*
+    const btn = document.querySelector('[data-show="button"]');
+
+    //записываем DOM элемент в переменную*
+    const parent = document.querySelector('[data-show="parent"]');
+
+    //событие клик по нашему DOM элементу*
+    btn.onclick = function() {
+
+      //элементу добавляем/удаляем класс (toggle)
+      //есть ещё add/remove, но toggle делает проверку
+      //если класса нет - добавляет, если есть - удаляет
+      parent.classList.toggle('is-open');
+      if (parent.classList.contains('is-open')) {
+        btn.innerHTML = 'Свернуть'
+      } else {
+        btn.innerHTML = 'Подробнее'
+      }
+    }
+
+
+  });
+
+});
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используется matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
